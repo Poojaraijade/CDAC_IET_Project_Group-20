43b9f58c3e2dde531db4da56e5f15a205b8b202e
Poojaraijade <106860616+Poojaraijade@users.noreply.github.com>
2022-09-07 19:14:02 +0530

    Pooja_Code_Modified(7/9/22:7.15pm)

---
+++ Backend RestApi/Pooja_Code_Modified/entity/TableRestaurantImageEntity.java
@@ -0,0 +1,70 @@
+package project.entity;
+/*
+import java.awt.image.BufferedImage;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class TableRestaurantImageEntity
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int restaurantImageId;
+	@Column
+	private BufferedImage restaurantImage;
+	@Column
+	private int restaurantInfoId;
+	
+	public TableRestaurantImageEntity()
+	{
+		
+	}
+	public TableRestaurantImageEntity(int restaurantImageId, BufferedImage restaurantImage, int restaurantInfoId)
+	{
+		super();
+		this.restaurantImageId = restaurantImageId;
+		this.restaurantImage = restaurantImage;
+		this.restaurantInfoId = restaurantInfoId;
+	}
+
+	public int getRestaurantImageId() 
+	{
+		return restaurantImageId;
+	}
+
+	public void setRestaurantImageId(int restaurantImageId) 
+	{
+		this.restaurantImageId = restaurantImageId;
+	}
+
+	public BufferedImage getRestaurantImage() 
+	{
+		return restaurantImage;
+	}
+
+	public void setRestaurantImage(BufferedImage restaurantImage) 
+	{
+		this.restaurantImage = restaurantImage;
+	}
+
+	public int getRestaurantInfoId() 
+	{
+		return restaurantInfoId;
+	}
+
+	public void setRestaurantInfoId(int restaurantInfoId) 
+	{
+		this.restaurantInfoId = restaurantInfoId;
+	}
+
+	@Override
+	public String toString() 
+	{
+		return "Table_Restaurant_Image [restaurantImageId=" + restaurantImageId + ", restaurantImage=" + restaurantImage
+				+ ", restaurantInfoId=" + restaurantInfoId + "]";
+	}
+}*/
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
