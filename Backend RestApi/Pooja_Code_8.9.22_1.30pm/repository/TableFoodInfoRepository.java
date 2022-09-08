c8d7e956c323c335af3cfeff74e1b6c573c1bbde
Poojaraijade <106860616+Poojaraijade@users.noreply.github.com>
2022-09-08 13:41:03 +0530

    Pooja_Code_8/9/22_1.30pm

---
+++ Backend RestApi/Pooja_Code_8.9.22_1.30pm/repository/TableFoodInfoRepository.java
@@ -0,0 +1,10 @@
+package project.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import project.entity.TableFoodInfoEntity;
+
+public interface TableFoodInfoRepository extends JpaRepository<TableFoodInfoEntity, Integer>
+{
+
+}
---
+++ Backend RestApi/Pooja_Code_8.9.22_1.30pm/repository/TableRestaurantImageRepository.java
@@ -0,0 +1,11 @@
+package project.repository;
+/*
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import project.entity.TableRestaurantImageEntity;
+
+public interface TableRestaurantImageRepository extends JpaRepository<TableRestaurantImageEntity, Integer>
+{
+
+}
+*/
