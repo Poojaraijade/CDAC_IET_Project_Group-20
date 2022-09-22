d9c9a5273f1a4f31d50be849d94e2b750b0b56dd
Ankit Raut <106860850+Iam-AnkitRaut@users.noreply.github.com>
2022-09-22 11:13:43 +0530

    Updated the Timeslot Buttons

--- DineEazy-UI/JS & CSS/EachRestroPage.js
+++ DineEazy-UI/JS & CSS/EachRestroPage.js
@@ -33,33 +33,143 @@ const EachRestroPage = props =>{
                                 </div>
 
                                 <div className="col-md-2"></div>
-                                <div className="col-md-2"></div>
-                                <div className="col-1">
-                                     <button className=" btn-primary w-100 py-1" type="11.00 AM">11.00 AM</button>
-                                </div>  
-                                <div className="col-1">
-                                    <button className=" btn-primary w-100 py-1" type="12.00 PM">12.00 PM</button>
-                                </div>
-                                <div className="col-1">
-                                    <button className=" btn-primary w-100 py-1" type="01.00 PM">01.00 PM</button>
-                                </div>
-                                <div className="col-1">
-                                    <button className=" btn-primary w-100 py-1" type="02.00 PM">02.00 PM</button>
-                                </div>
-                                <div className="col-1">
-                                    <button className=" btn-primary w-100 py-1" type="11.00 AM">08.00 AM</button>
-                                </div>  
-                                <div className="col-1">
-                                    <button className=" btn-primary w-100 py-1" type="12.00 PM">10.00 PM</button>
-                                </div>
-                                <div className="col-1">
-                                    <button className=" btn-primary w-100 py-1" type="07.00 PM">07.00 PM</button>
-                                </div>
-                                <div className="col-1">
-                                     <button className=" btn-primary w-100 py-1" type="05.00 PM">05.00 PM</button>
-                           </div>                                        
-                             
-                
+                               
+                                   {/*****************************************************************************************************************/}
+                       
+                       <div className="col-md-2"></div>
+                       <div className="col-md-2"></div>
+                       <div className="col-md-2" >    
+                       <div class="dropdown">
+                       
+                         <button  class="btn btn-secondary dropdown-toggle w-100 py-1" style={{backgroundColor:"#FEA116"}} type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false" >&ensp;&ensp;Break Fast&ensp;&ensp;</button>
+                         <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                           <li>
+                              <a class="dropdown-item">
+                                 <input class="form-check-input" name="a" type="radio"  value="7.00 AM" />
+                                 <label class="form-check-label">&ensp; 7.00 AM &ensp;</label>
+                             </a>
+                           </li>
+                           <li>
+                              <a class="dropdown-item">
+                                 <input class="form-check-input"  name="a" type="radio" value="8.00 AM" />
+                                 <label class="form-check-label">&ensp; 8.00 AM &ensp;</label>
+                             </a>
+                           </li>
+                           <li>
+                              <a class="dropdown-item">
+                                 <input class="form-check-input"  name="a" type="radio" value="9.00 AM " />
+                                 <label class="form-check-label">&ensp; 9.00 AM &ensp;</label>
+                             </a>
+                           </li>
+                           <li>
+                              <a class="dropdown-item">
+                                 <input class="form-check-input"  name="a" type="radio" value="10.00 AM " />
+                                 <label class="form-check-label">&ensp; 10.00 AM &ensp;</label>
+                             </a>
+                           </li>
+                           <li>
+                              <a class="dropdown-item">
+                                 <input class="form-check-input"  name="a" type="radio" value="11.00 AM" />
+                                 <label class="form-check-label">&ensp; 11.00 AM &ensp;</label>
+                             </a>
+                           </li>
+                         </ul>
+                       </div>
+                       </div>
+
+                        {/******************************************************************************************************/}
+                     <div className="col-md-1"></div>  
+                     <div className="col-md-2" >    
+                        <div class="dropdown">
+                         <button class="btn btn-secondary dropdown-toggle w-100 py-1" style={{backgroundColor:"#FEA116"}} type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false" >&ensp;&ensp;&ensp;Lunch&ensp;&ensp;&ensp;&ensp;</button>
+                         <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                           <li>
+                              <a class="dropdown-item">
+                                 <input class="form-check-input" type="radio" name="a" value="12.00 PM" />
+                                 <label class="form-check-label">&ensp; 12.00 PM &ensp;</label>
+                             </a>
+                           </li>
+                           <li>
+                              <a class="dropdown-item">
+                                 <input class="form-check-input" type="radio" name="a" value="1.00 PM" />
+                                 <label class="form-check-label">&ensp; 1.00 PM &ensp;</label>
+                             </a>
+                           </li>
+                           <li>
+                              <a class="dropdown-item">
+                                 <input class="form-check-input" type="radio" name="a" value="2.00 PM" />
+                                 <label class="form-check-label">&ensp; 2.00 PM &ensp;</label>
+                             </a>
+                           </li>
+                           <li>
+                              <a class="dropdown-item">
+                                 <input class="form-check-input" type="radio" name="a" value="3.00 PM" />
+                                 <label class="form-check-label">&ensp; 3.00 PM &ensp;</label>
+                             </a>
+                           </li>
+                           <li>
+                              <a class="dropdown-item">
+                                 <input class="form-check-input" type="radio" name="a" value="4.00 PM" />
+                                 <label class="form-check-label">&ensp; 4.00 PM &ensp;</label>
+                             </a>
+                           </li>
+                           <li>
+                              <a class="dropdown-item">
+                                 <input class="form-check-input" type="radio" name="a" value="5.00 PM" />
+                                 <label class="form-check-label">&ensp; 5.00 PM &ensp;</label>
+                             </a>
+                           </li>
+                         </ul>
+                       </div>
+                       </div>
+                       
+                       {/*******************************************************************************************/}
+                       <div className="col-md-1"></div>
+                       <div className="col-md-2" >    
+                        <div class="dropdown">
+                         <button    class="btn btn-secondary dropdown-toggle w-100 py-1" style={{backgroundColor:"#FEA116"}} type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false" >&ensp;&ensp;&ensp;Dinner&ensp;&ensp;&ensp;&ensp;</button>
+                         <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                           <li>
+                              <a class="dropdown-item">
+                                 <input class="form-check-input" type="radio" name="a" value="6.00 PM" />
+                                 <label class="form-check-label">&ensp; 6.00 PM &ensp;</label>
+                             </a>
+                           </li>
+                           <li>
+                              <a class="dropdown-item">
+                                 <input class="form-check-input" type="radio" name="a" value="7.00 PM" />
+                                 <label class="form-check-label">&ensp; 7.00 PM &ensp;</label>
+                             </a>
+                           </li>
+                           <li>
+                              <a class="dropdown-item">
+                                 <input class="form-check-input" type="radio" name="a" value="8.00 PM " />
+                                 <label class="form-check-label">&ensp; 8.00 PM &ensp;</label>
+                             </a>
+                           </li>
+                           <li>
+                              <a class="dropdown-item">
+                                 <input class="form-check-input" type="radio" name="a" value="9.00 PM " />
+                                 <label class="form-check-label">&ensp; 9.00 PM &ensp;</label>
+                             </a>
+                           </li>
+                           <li>
+                              <a class="dropdown-item">
+                                 <input class="form-check-input" type="radio" name="a" value="10.00 PM " />
+                                 <label class="form-check-label">&ensp; 10.00 PM &ensp;</label>
+                             </a>
+                           </li>
+                           <li>
+                              <a class="dropdown-item">
+                                 <input class="form-check-input" type="radio" name="a" value="11.00 PM" />
+                                 <label class="form-check-label">&ensp; 11.00 PM &ensp;</label>
+                             </a>
+                           </li>
+                         </ul>
+                       </div>
+                       </div>
+                    
+                    {/*******************************************************************************************/}
 
 
                                 <div className="col-md-2"></div>
